79c7823f8e935a9e5bb4cf762d17a365807863eb
nki1121 <nkiuniv@gmail.com>
2024-03-21 01:38:56 +0900

    Initial Commit

---
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by 中井晋平 on 2024/03/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
